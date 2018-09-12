55fe0f831ca55f9c8e7832ea6503f510717a5a0d
davide <dav.valiante@gmail.com>
2018-09-12 15:43:56 +0200

    Riportato layout slot a quello fico, modificata card nell'RC della home, preparazione release

--- app/src/main/java/com/musashi/claymore/spike/spike/App.kt
+++ app/src/main/java/com/musashi/claymore/spike/spike/App.kt
@@ -3,6 +3,7 @@ package com.musashi.claymore.spike.spike
 import android.app.Application
 import android.support.multidex.MultiDex
 import com.google.firebase.database.FirebaseDatabase
+import com.squareup.leakcanary.LeakCanary
 
 
 class App : Application() {
@@ -10,6 +11,13 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
+        // Normal app init code...
         MultiDex.install(this)
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
     }
